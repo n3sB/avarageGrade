f517f85c59c9080b0d5d603d77f7c166d5f592f5
N3sB <enesbaysl@outlook.com>
2022-02-15 20:17:27 +0300

    Avarage Grade

---
+++ JAVA101/avarageGrade.java
@@ -0,0 +1,39 @@
+package JAVA101;
+
+import java.util.Scanner;
+
+public class avarageGrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Please enter your Physics grade: ");
+    double physics = scanner.nextDouble();
+    System.out.println(" ");
+
+    System.out.print("Please enter your Chemistry grade: ");
+    double chemistry = scanner.nextDouble();
+    System.out.println(" ");
+   
+    System.out.print("Please enter your Turkish grade: ");
+    double turkish = scanner.nextDouble();
+    System.out.println(" ");
+    
+    System.out.print("Please enter your History grade: ");
+    double history = scanner.nextDouble();
+    System.out.println(" ");
+    
+    System.out.print("Please enter your Music grade: ");
+    double music = scanner.nextDouble();
+    System.out.println(" ");
+    
+    
+    double avarageGrade = (physics+chemistry+turkish+history+music)/5;
+    scanner.close();
+
+    boolean condition = avarageGrade > 60;
+
+    String passedOrFailed = (condition) ? "Sınıfı " + avarageGrade + " İle Geçti" :"Sınıfta Kaldı";
+
+        System.out.println(passedOrFailed);
+    }
+}
---
+++ JAVA101/java1.java
@@ -0,0 +1,29 @@
+package JAVA101;
+
+import java.util.Scanner;
+
+public class java1 {
+    public static void main(String[] args) {
+      
+      /*  int a = 123;
+        int sd = 23;
+
+        sd *= a;
+        System.out.println(sd);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your age: ");
+        int scanner1 = scanner.nextInt();
+        System.out.println("Your age is " + scanner1);
+        scanner.close();
+    */
+        int number1 = 5;
+        int number2 = 4;
+        int number3 = 12;
+        boolean condition1 = number1 == number2;
+        boolean condition2 = number1 >= number3;
+        boolean totalCondition = condition1 || condition2;
+
+        String str = (totalCondition) ? "true": "false"; 
+        System.out.println(totalCondition);
+    }
+}
