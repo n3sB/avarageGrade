a4633344be723af8fd205f2eb63e61bb9347e5d1
N3sB <enesbaysl@outlook.com>
2022-02-19 14:02:35 +0300

    Update averageGrade.java

--- averageGrade.java
+++ averageGrade.java
@@ -4,33 +4,32 @@ public class averageGrade {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-    System.out.print("Please enter your Physics grade: ");
-    double physics = scanner.nextDouble();
-    System.out.println(" ");
+        System.out.print("Please enter your Physics grade: ");
+        double physics = scanner.nextDouble();
+        System.out.println(" ");
 
-    System.out.print("Please enter your Chemistry grade: ");
-    double chemistry = scanner.nextDouble();
-    System.out.println(" ");
-   
-    System.out.print("Please enter your Turkish grade: ");
-    double turkish = scanner.nextDouble();
-    System.out.println(" ");
-    
-    System.out.print("Please enter your History grade: ");
-    double history = scanner.nextDouble();
-    System.out.println(" ");
-    
-    System.out.print("Please enter your Music grade: ");
-    double music = scanner.nextDouble();
-    System.out.println(" ");
-    
-    
-    double averageGrade = (physics+chemistry+turkish+history+music)/5;
-    scanner.close();
+        System.out.print("Please enter your Chemistry grade: ");
+        double chemistry = scanner.nextDouble();
+        System.out.println(" ");
 
-    boolean condition = averageGrade > 60;
+        System.out.print("Please enter your Turkish grade: ");
+        double turkish = scanner.nextDouble();
+        System.out.println(" ");
 
-    String passedOrFailed = (condition) ? "Sınıfı " + averageGrade + " İle Geçti" :"Sınıfta Kaldı";
+        System.out.print("Please enter your History grade: ");
+        double history = scanner.nextDouble();
+        System.out.println(" ");
+
+        System.out.print("Please enter your Music grade: ");
+        double music = scanner.nextDouble();
+        System.out.println(" ");
+
+        double averageGrade = (physics + chemistry + turkish + history + music) / 5;
+        scanner.close();
+
+        boolean condition = averageGrade > 60;
+
+        String passedOrFailed = (condition) ? "Sınıfı " + averageGrade + " İle Geçti" : "Sınıfta Kaldı";
 
         System.out.println(passedOrFailed);
     }
